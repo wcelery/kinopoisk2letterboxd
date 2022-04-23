32f624b7876984c4cf987ece4b8de27f7b927186
wcelery <horoschun.yura@yandex.ru>
2022-04-23 15:55:05 +0800

    Simplify appending algorithm

--- kinopoisk2letterboxd.py
+++ kinopoisk2letterboxd.py
@@ -2,11 +2,8 @@ import pandas as pd
 import html
 
 k_import_filename = ""
-k_import = open(k_import_filename, "r", encoding='Windows-1251')
+k_import = open(k_import_filename, "r", encoding='utf-8')
 
-#Get rid of first intro part
-for i in range(33):
-	line = k_import.readline()
 
 '''
 0 - Title
@@ -21,15 +18,40 @@ line_number = 0
 Title_flag = 0
 Year_flag = 0
 WatchedDate_flag = 0
-Rating10_flag = 0
+
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
 
 for line in k_import:
-
     line_number += 1
     movie_number = line_number//24 + 1
 
+    # movie title starts after 5th comma, + 1 to exclude comma from the title
+    movie_rating_index_start = find_nth(line, ',', 4) + 1
+    movie_title_index_start = find_nth(line, ',', 5) + 1
+
+    # movie title ends after 2th comma from the end
+    movie_title_index_end = find_nth(line, ',', 6)
+
+    movie_title = line[movie_title_index_start:movie_title_index_end]
+
+    # append title
+    movie_list.append([html.unescape(movie_title),'','', ''])
+
+    # append watched date
+    movie_list[line_number][2] = line[7:11]+'-'+line[4:6]+'-'+line[1:3]
+
+    # append rating
+
+    movie_list[line_number][3] = line[movie_rating_index_start:movie_title_index_start-1] 
+    print(movie_list[line_number])
+    # todo organize the code
     #Title
-    if (line_number%24)%4 == 0 and (line_number%24 != 0) and Title_flag < movie_number:
+    '''if (line_number%24)%4 == 0 and (line_number%24 != 0) and Title_flag < movie_number:
         Title_flag += 1
         
         #Is title in english or in russian?
@@ -51,7 +73,7 @@ for line in k_import:
         movie_list[movie_number][3] = line[32:-6]
 
     #If there's only russian title
-    previous_line = line
+    previous_line = line'''
 
 del movie_list[-1]
 movie_df = pd.DataFrame(movie_list)
